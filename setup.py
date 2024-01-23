bde523f8de499b4868514a8167988ed3e2865015
Dimitri Papaioannou <algorythmist@users.noreply.github.com>
2024-01-23 15:56:20 -0800

    build tools

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='quantum-machine-learning',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/algorythmist/quantum-machine-learning',
+    author='Dimitri Papaioannou',
+    description='Quantum Machine Learning Toolkits',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10'
+)
