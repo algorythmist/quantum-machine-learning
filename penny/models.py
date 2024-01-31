81657006f6b532c556c1a667d373ecff25261143
Dimitri Papaioannou <algorythmist@users.noreply.github.com>
2024-01-31 13:48:39 -0800

    added demo

--- penny/models.py
+++ penny/models.py
@@ -15,10 +15,19 @@ class QMLModel(ABC):
         pass
 
 
+def angle_embedding_fn(features, num_qubits):
+    return qml.AngleEmbedding(features, wires=range(num_qubits))
+
+
+def amplitude_embedding_fn(features, num_qubits):
+    return qml.AmplitudeEmbedding(features, wires=range(num_qubits),
+                                  pad_with=0.0, normalize=True)
+
+
 class StronglyEntangledBinaryModel(QMLModel):
 
     def __init__(self, num_qubits: int,
-                 embedding_fn=qml.AngleEmbedding):
+                 embedding_fn=angle_embedding_fn):
         self.num_qubits = num_qubits
         self.embedding = embedding_fn
 
@@ -26,19 +35,20 @@ class StronglyEntangledBinaryModel(QMLModel):
         return self.num_qubits
 
     def evaluate(self, weights, features):
-        self.embedding(features, wires=range(self.num_qubits))
+        self.embedding(features, self.num_qubits)
         qml.StronglyEntanglingLayers(weights, wires=range(self.num_qubits))
         return qml.expval(qml.PauliZ(0))
 
 
 class MultiClassQMLModel(QMLModel):
 
-    def __init__(self, num_qubits, num_classes, embedding=qml.AngleEmbedding):
+    def __init__(self, num_qubits, num_classes,
+                 embedding_fn=angle_embedding_fn):
         if num_qubits < num_classes:
             raise ValueError("num_qubits must be greater than or equal to num_classes")
         self.num_qubits = num_qubits
         self.num_classes = num_classes
-        self.embedding = embedding
+        self.embedding = embedding_fn
 
     def layer(self, W):
         for i in range(self.num_qubits):
@@ -48,7 +58,7 @@ class MultiClassQMLModel(QMLModel):
             qml.CNOT(wires=[i, (i + 1) % self.num_qubits])
 
     def evaluate(self, weights, features):
-        self.embedding(features, wires=range(self.num_qubits))
+        self.embedding(features, self.num_qubits)
         for W in weights:
             self.layer(W)
         return [qml.expval(qml.PauliZ(i)) for i in range(self.num_classes)]
--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -2,21 +2,24 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 
 from penny.classifier import *
-from penny.models import StronglyEntangledBinaryModel
+from penny.models import StronglyEntangledBinaryModel, amplitude_embedding_fn
 
-
-def test_binary_iris():
-    random_seed=623
-    pnp.random.seed(random_seed)
-
+def get_binary_scaled_iris():
     X, y = load_iris(return_X_y=True)
     features = X[y != 2]
     labels = y[y != 2]
     labels = np.where(labels == 0, -1, 1)
     features_scaled = StandardScaler().fit_transform(features)
+    return labels, features_scaled
+
+def test_binary_iris():
+    random_seed=623
+    pnp.random.seed(random_seed)
+
+    labels, features_scaled = get_binary_scaled_iris()
     X_train, X_test, y_train, y_test = train_test_split(features_scaled, labels, shuffle=True, test_size=0.2,
                                                         random_state=random_seed)
 
@@ -32,3 +35,22 @@ def test_binary_iris():
     assert accuracy == 1.0
 
 
+def test_amplitude_embedding_iris():
+    random_seed = 12939
+    pnp.random.seed(random_seed)
+
+    labels, features_scaled = get_binary_scaled_iris()
+    X_train, X_test, y_train, y_test = train_test_split(features_scaled, labels, shuffle=True, test_size=0.2,
+                                                        random_state=random_seed)
+    qubits = 2
+    classifier = BinaryClassifier(
+        model=StronglyEntangledBinaryModel(qubits,
+                                           embedding_fn=amplitude_embedding_fn),
+        weights_shape=(3, qubits, 3),
+        iterations=100
+    )
+
+    classifier.fit(X_train, y_train)
+    predictions = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions)
+    print(accuracy)
