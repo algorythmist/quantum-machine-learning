40d534913211609d3112bb77c81be1ae53cf76ad
Dimitri Papaioannou <algorythmist@users.noreply.github.com>
2024-02-01 15:20:39 -0800

    added gnomonic

--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -5,5 +5,10 @@ from pytest import approx
 def test_stereographic():
     X = np.array([0.3, 0.7], dtype=float)
     P = inverse_stereographic_projection(X)
-    print(P)
     assert np.sum(P**2) == approx(1.00)
+
+
+def test_gnomonic():
+    X = np.array([0.3, 0.7], dtype=float)
+    P = inverse_gnomonic_projection(X)
+    assert np.sum(P ** 2) == approx(1.00)
--- utils/embeddings.py
+++ utils/embeddings.py
@@ -4,3 +4,9 @@ import numpy as np
 def inverse_stereographic_projection(X):
     s2 = np.sum(np.square(X))
     return np.hstack([(s2-1)/(s2+1), 2*X/(s2+1)])
+
+
+def inverse_gnomonic_projection(X):
+    embedded = [1] + X
+    norm = np.linalg.norm(embedded)
+    return embedded / norm
