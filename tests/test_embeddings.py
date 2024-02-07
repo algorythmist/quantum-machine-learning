6809dce4e2a4dff3cbeeac3842a052863845c039
Dimitri Papaioannou <algorythmist@users.noreply.github.com>
2024-02-07 08:56:44 -0800

    added projection variants

--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -3,12 +3,23 @@ from pytest import approx
 
 
 def test_stereographic():
-    X = np.array([0.3, 0.7], dtype=float)
+    X = np.array([1.2, 0.3, -0.7], dtype=float)
     P = inverse_stereographic_projection(X)
+    assert len(P) == 4
     assert np.sum(P**2) == approx(1.00)
 
 
 def test_gnomonic():
-    X = np.array([0.3, 0.7], dtype=float)
-    P = inverse_gnomonic_projection(X)
-    assert np.sum(P ** 2) == approx(1.00)
+    X = np.array([1.2, 0.3, -0.7], dtype=float)
+    P = arbitrary_inverse_projection(0, X)
+    p = np.array(P)
+    assert np.sum(p ** 2) == approx(1.00)
+    assert len(p) == 4
+
+
+def test_twilight():
+    X = np.array([1.2, 0.3, -0.7], dtype=float)
+    P = arbitrary_inverse_projection(-1-np.sqrt(2)/2, X)
+    p = np.array(P)
+    assert np.sum(p ** 2) == approx(1.00)
+    assert len(p) == 4
--- utils/embeddings.py
+++ utils/embeddings.py
@@ -3,10 +3,23 @@ import numpy as np
 
 def inverse_stereographic_projection(X):
     s2 = np.sum(np.square(X))
-    return np.hstack([(s2-1)/(s2+1), 2*X/(s2+1)])
+    return np.hstack([(s2 - 1) / (s2 + 1), 2 * X / (s2 + 1)])
 
 
-def inverse_gnomonic_projection(X):
-    embedded = [1] + X
-    norm = np.linalg.norm(embedded)
-    return embedded / norm
+def arbitrary_inverse_projection(a, x_dash_in_Rn):
+    # a should be the x_0 coordinate of the centre of projection. x should be a list of length n, corresponding to (x_1', ... , x_n') as in the diagram
+    s = np.linalg.norm(x_dash_in_Rn)
+
+    # New part - if a <-1, then this extends the domain of the projection to the entirety of Rn
+    if (a < -1) and (s > np.sqrt((a - 1) / (a + 1))):
+        x_dash_in_Rn = [np.sqrt((a - 1) / (a + 1)) * x_i / s for x_i in x_dash_in_Rn]
+        s = np.sqrt((a - 1) / (a + 1))
+
+    # quadratic formula
+    A = (s ** 2 + (1 - a) ** 2)
+    B = -2 * a * s ** 2
+    C = s ** 2 * a ** 2 - (1 - a) ** 2
+
+    x_0 = (-B + np.sqrt(B ** 2 - 4 * A * C)) / (2 * A)
+
+    return [x_0] + [((x_0 - a) / (1 - a)) * x_i for x_i in x_dash_in_Rn]
